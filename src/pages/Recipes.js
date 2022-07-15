1b35f93b8fbd2f98216b567c132829aa5eeada12
Sérgio Cassette <sergiocassette@gmail.com>
2022-07-15 17:27:22 -0300

    req 19 ainda com erros

--- src/pages/Recipes.js
+++ src/pages/Recipes.js
@@ -1,22 +1,27 @@
-import React from 'react';
-import dataProvistorio from '../dateProvisorio';
+import React, { useContext } from 'react';
+import RecipesContext from '../context/RecipesContext';
 
 function Recipes() {
-  console.log(dataProvistorio);
   const nrDeReceitas = 12;
+  const { mealsApi, loading } = useContext(RecipesContext);
   return (
     <header>
-      { dataProvistorio.slice(0, nrDeReceitas).map((food, index) => (
-        <div key={ index } data-testid={ `${index}-recipe-card` }>
-          <h1>{ food.strMeal }</h1>
-          <img
-            src={ food.strMealThumb }
-            alt={ food.strMeal }
-            data-testid={ `${index}-card-img` }
-          />
-        </div>
-      )) }
-
+      { loading ? <p>Loading</p> : (
+        <section>
+          { mealsApi.slice(0, nrDeReceitas).map((food, index) => (
+            <div key={ food.idMeal } data-testid={ `${index}-recipe-card` }>
+              <h1 data-testid={ `${index}-card-name` }>{ food.strMeal }</h1>
+              <img
+                src={ food.strMealThumb }
+                alt={ food.strMeal }
+                data-testid={ `${index}-card-img` }
+                width="20"
+                height="20"
+              />
+            </div>
+          )) }
+        </section>
+      ) }
     </header>
   );
 }
