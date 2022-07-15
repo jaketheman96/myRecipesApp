538cd3d11f7ccb316ebd86c4bdf35594f06b992c
Jake Chien <jake_chien@outlook.com>
2022-07-15 15:44:34 -0300

    Adiciona funcao fetch de meals e drinks

--- src/context/RecipesProvider.js
+++ src/context/RecipesProvider.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import RecipesContext from './RecipesContext';
 
@@ -6,6 +6,25 @@ function RecipesProvider({ children }) {
   const [userEmail, setUserEmail] = useState('');
   const [password, setPassword] = useState('');
   const [disabled, setDisabled] = useState(true);
+  const [mealsApi, setMealsApi] = useState([]);
+  const [drinksApi, setDrinksApi] = useState([]);
+
+  useEffect(() => {
+    const fetchApiMeals = async () => {
+      const url = 'https://www.themealdb.com/api/json/v1/1/search.php?s=';
+      const fetching = await fetch(url);
+      const response = await fetching.json();
+      setMealsApi(response);
+    };
+    const fetchApiDrinks = async () => {
+      const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
+      const fetching = await fetch(url);
+      const response = await fetching.json();
+      setDrinksApi(response);
+    };
+    fetchApiMeals();
+    fetchApiDrinks();
+  }, []);
 
   const STATE = {
     userEmail,
@@ -14,6 +33,8 @@ function RecipesProvider({ children }) {
     setPassword,
     disabled,
     setDisabled,
+    mealsApi,
+    drinksApi,
   };
 
   return (
