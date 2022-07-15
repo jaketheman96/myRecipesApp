76aad5332da91e9588ab3186e7ab46ae69a17f67
Sérgio Cassette <sergiocassette@gmail.com>
2022-07-15 15:47:31 -0300

    Req 19 - pausa para atualização

--- src/pages/Recipes.js
+++ src/pages/Recipes.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import dataProvistorio from '../dateProvisorio';
 
 function Recipes() {
+  console.log(dataProvistorio);
+  const nrDeReceitas = 12;
   return (
-    <div>Tela de Receitas</div>
+    <header>
+      { dataProvistorio.slice(0, nrDeReceitas).map((food, index) => (
+        <div key={ index } data-testid={ `${index}-recipe-card` }>
+          <h1>{ food.strMeal }</h1>
+          <img
+            src={ food.strMealThumb }
+            alt={ food.strMeal }
+            data-testid={ `${index}-card-img` }
+          />
+        </div>
+      )) }
+
+    </header>
   );
 }
 
