01bb9fd9a9e05a8a47aa17d64e8253bfa10526e8
Jake Chien <jake_chien@outlook.com>
2022-07-19 17:28:36 -0300

    Requisito 19 :thumbsup:

--- src/components/MainDrink.js
+++ src/components/MainDrink.js
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import RecipesContext from '../context/RecipesContext';
 
 function MainDrink() {
+  const { drinkData: { drinks } } = useContext(RecipesContext);
+  const nrDeReceitas = 12;
+
   return (
-    <div>main drink</div>
+    <div>
+      {drinks && drinks.slice(0, nrDeReceitas).map((element, index) => (
+        <div key={ element.idDrink } data-testid={ `${index}-recipe-card` }>
+          <h1 data-testid={ `${index}-card-name` }>{ element.strDrink }</h1>
+          <img
+            src={ element.strDrinkThumb }
+            alt={ element.strDrink }
+            data-testid={ `${index}-card-img` }
+            width="50"
+            height="50"
+          />
+        </div>
+      ))}
+    </div>
   );
 }
 
