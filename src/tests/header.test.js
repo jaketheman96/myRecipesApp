192f9d19fc6e767f8efbff019713a114e9046d8d
Diego Moraes Cezar <dmcezar@gmail.com>
2022-07-21 21:58:11 -0300

    Cobertura Total: 75.7%. Escrevendo testes para as linhas 32,37-50,58-59,82-92,104 do componente SearchBar.

--- src/tests/header.test.js
+++ src/tests/header.test.js
@@ -1,11 +1,12 @@
 import { screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event'
 import renderWithRoute from './helpers/renderWithRoute';
-import { tomatoMock } from './mocks/mock';
+import { tomatoMock, aLetterMock, dLetterMock } from './mocks/mock';
 
 describe('Testes do componente Header:', () => {
+  beforeEach(() => renderWithRoute('/foods'));
+
   it('1. A barra de busca aparece e some quando o botão de busca é clicado;', () => {
-    renderWithRoute('/foods');
     userEvent.click(screen.getByTestId('search-top-btn'));
     const searchBar = screen.getByTestId('search-input');
     expect(searchBar).toBeInTheDocument();
@@ -16,8 +17,6 @@ describe('Testes do componente Header:', () => {
   });
 
   it('2. A busca retorna uma receita específica;', async () => {
-    renderWithRoute('/foods');
-
     userEvent.click(screen.getByTestId('search-top-btn'));
     const searchBar = screen.getByTestId('search-input');
     const nameRadio = screen.getByTestId('name-search-radio');
@@ -35,7 +34,6 @@ describe('Testes do componente Header:', () => {
   });
 
   it('3. A busca retorna apenas receitas com um determinado ingrediente;', async () => {
-    renderWithRoute('/foods');
     userEvent.click(screen.getByTestId('search-top-btn'));
     const searchBar = screen.getByTestId('search-input');
     const ingredientRadio = screen.getByTestId('ingredient-search-radio');
@@ -48,9 +46,74 @@ describe('Testes do componente Header:', () => {
     await waitFor(() => {
       tomatoMock.forEach((name) => expect(screen.getByText(name)).toBeInTheDocument());
     }, { timeout: 3000 });
+  });
 
-    /* tomatoMock.forEach((name) => expect(screen.getByText(name)).toBeInTheDocument()); */
+  it('4. A busca retorna apenas receitas que comecem com uma determinada letra;', async () => {
+    userEvent.click(screen.getByTestId('search-top-btn'));
+    const searchBar = screen.getByTestId('search-input');
+    const firstLetterRadio = screen.getByTestId('first-letter-search-radio');
+    const searchBtn = screen.getByTestId('exec-search-btn');
 
-  })
+    userEvent.type(searchBar, 'a');
+    userEvent.click(firstLetterRadio);
+    userEvent.click(searchBtn);
 
+    await waitFor(() => {
+      aLetterMock.forEach((name) => expect(screen.getByText(name)).toBeInTheDocument());
+    }, { timeout: 3000 });
+
+    /* dLetterMock.forEach((name) => expect(screen.getByText(name)).not.toBeInTheDocument()); */
+
+    userEvent.clear(searchBar);
+    userEvent.type(searchBar, 'd');
+    userEvent.click(searchBtn);
+
+    await waitFor(() => {
+      dLetterMock.forEach((name) => expect(screen.getByText(name)).toBeInTheDocument());
+    }, { timeout: 3000 });
+
+
+
+  });
+
+  it('5. A busca retorna um alerta caso não encontre resultados;', async () => {
+    userEvent.click(screen.getByTestId('search-top-btn'));
+    const searchBar = screen.getByTestId('search-input');
+    const ingredientRadio = screen.getByTestId('ingredient-search-radio');
+    const searchBtn = screen.getByTestId('exec-search-btn');
+    const alertMock = jest.spyOn(window, 'alert');
+
+    userEvent.type(searchBar, 'pleh');
+    userEvent.click(ingredientRadio);
+    userEvent.click(searchBtn);
+
+    await waitFor(() => {
+      expect(alertMock).toHaveBeenCalledTimes(1);
+    }, { timeout: 3000 });
+  });
+
+  it('6. A busca retorna um alerta caso mais de uma letra seja fornecida ao filtrar pela primeira letra;', async () => {
+    userEvent.click(screen.getByTestId('search-top-btn'));
+    const searchBar = screen.getByTestId('search-input');
+    const firstLetterRadio = screen.getByTestId('first-letter-search-radio');
+    const searchBtn = screen.getByTestId('exec-search-btn');
+    const alertMock = jest.spyOn(window, 'alert');
+
+    userEvent.type(searchBar, 'pleh');
+    userEvent.click(firstLetterRadio);
+    userEvent.click(searchBtn);
+
+    await waitFor(() => {
+      expect(alertMock).toHaveBeenCalledTimes(1);
+    }, { timeout: 3000 });
+  });
+
+  it('7. Possui um link que redireciona para a página de perfil;', async () => {
+    const profileLink = screen.getByTestId('profile-top-btn');
+    userEvent.click(profileLink);
+
+    await waitFor(() => {
+      expect(screen.getByText('Profile')).toBeInTheDocument();
+    }, { timeout: 3000 });
+  });
 });
--- src/tests/mocks/mock.js
+++ src/tests/mocks/mock.js
@@ -1,1 +1,5 @@
 export const tomatoMock = ['Brown Stew Chicken', 'Callaloo Jamaican Style', 'Chivito uruguayo', 'Kapsalon', 'Lasagna Sandwiches', 'Matar Paneer', 'Moussaka', 'Ratatouille', 'Vegetarian Casserole'];
+
+export const aLetterMock = ['Apple Frangipan Tart', 'Apple & Blackberry Crumble', 'Apam balik', 'Ayam Percik'];
+
+export const dLetterMock = ['Dal fry', 'Dundee cake', 'Duck Confit'];
