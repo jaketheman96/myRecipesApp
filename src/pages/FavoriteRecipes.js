0f46869d6f98998ced1265aeeb70d7393df8aeb6
Sérgio Cassette <sergiocassette@gmail.com>
2022-07-26 15:58:58 -0300

    requisitos 50 a 56 faltando test

--- src/pages/FavoriteRecipes.js
+++ src/pages/FavoriteRecipes.js
@@ -1,10 +1,145 @@
-import React from 'react';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import Header from '../components/header/Header';
+import shareIcon from '../images/shareIcon.svg';
+import blackHeartIcon from '../images/blackHeartIcon.svg';
+
+const copy = require('clipboard-copy');
 
 function FavoriteRecipes() {
+  const initialFavoriteRecipes = JSON.parse(localStorage.favoriteRecipes);
+  console.log(initialFavoriteRecipes);
+
+  const [copySuccess, setCopySuccess] = useState('');
+  const [favoriteAll, setFavoriteAll] = useState(initialFavoriteRecipes);
+
+  const handleShareClick = (event) => {
+    copy(`http://localhost:3000/${event.target.value}`);
+    setCopySuccess('Link copied!');
+  };
+
+  const handFilterFood = () => {
+    const initialFavoriteRecipes1 = JSON.parse(localStorage.favoriteRecipes);
+    const foodFilter = initialFavoriteRecipes1.filter((r) => r.type !== 'drink');
+    console.log('foodFilter: ', foodFilter);
+    setFavoriteAll(foodFilter);
+  };
+
+  const handFilterDrink = () => {
+    const initialFavoriteRecipes2 = JSON.parse(localStorage.favoriteRecipes);
+    const drinkFilter = initialFavoriteRecipes2.filter((r) => r.type !== 'food');
+    console.log('drinkFilter: ', drinkFilter);
+    setFavoriteAll(drinkFilter);
+  };
+
+  const handFilterAll = () => {
+    const initialFavoriteRecipes3 = JSON.parse(localStorage.favoriteRecipes);
+    console.log('initialFavoriteRecipes: ', initialFavoriteRecipes3);
+    setFavoriteAll(initialFavoriteRecipes3);
+  };
+
+  const handleFavoriteClick = (event) => {
+    const newFavorite = initialFavoriteRecipes.filter(
+      (recipe) => recipe.id !== event.target.value,
+    );
+    localStorage.favoriteRecipes = JSON.stringify(newFavorite);
+    setFavoriteAll(favoriteAll.filter((recipe) => recipe.id !== event.target.value));
+  };
+
   return (
-    <Header />
+    <div>
+      <Header />
+      <h1>Favorite Recipes</h1>
+      <button
+        type="button"
+        data-testid="filter-by-all-btn"
+        onClick={ handFilterAll }
+      >
+        All
+
+      </button>
+      <button
+        type="button"
+        data-testid="filter-by-food-btn"
+        onClick={ handFilterFood }
+      >
+        Food
+
+      </button>
+      <button
+        type="button"
+        data-testid="filter-by-drink-btn"
+        onClick={ handFilterDrink }
+      >
+        Drink
+
+      </button>
+      { favoriteAll ? favoriteAll.map((element, index) => (
+        <>
+          <div>
+            <input
+              type="image"
+              src={ shareIcon }
+              alt="Share Button"
+              name="share-btn"
+              value={ `${element.type}s/${element.id}` }
+              data-testid={ `${index}-horizontal-share-btn` }
+              onClick={ handleShareClick }
+            />
+            { copySuccess }
+            <input
+              type="image"
+              src={ blackHeartIcon }
+              alt="Favorite Button"
+              value={ element.id }
+              name="favorite-btn"
+              data-testid={ `${index}-horizontal-favorite-btn` }
+              onClick={ handleFavoriteClick }
+            />
+          </div>
+          <Link
+            to={ `/${element.type}s/${element.id}` }
+            key={ element.id }
+          >
+            <h3 data-testid={ `${index}-horizontal-name` }>{element.name}</h3>
+            <div key={ element.id } data-testid={ `${index}-horizontal-top-text` }>
+              {element.type === 'food' ? (
+                <p>
+                  {element.nationality}
+                  {' '}
+                  -
+                  {' '}
+                  {element.category}
+                </p>
+              ) : (
+                <p>
+                  {element.alcoholicOrNot}
+                </p>
+              ) }
+              <img
+                src={ element.image }
+                alt={ element.name }
+                data-testid={ `${index}-horizontal-image` }
+                width="50"
+                height="50"
+              />
+            </div>
+          </Link>
+
+        </>
+      )) : null }
+    </div>
   );
 }
+
+FavoriteRecipes.propTypes = {
+  match: PropTypes.shape({
+    url: PropTypes.string.isRequired,
+    params: PropTypes.shape(
+      PropTypes.string.isRequired,
+    ).isRequired,
+  }).isRequired,
+};
 
 export default FavoriteRecipes;
